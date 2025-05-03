6a781e8463a25f67318837bcf3f0768311221203
Sergei Korablev <jres.kor@gmail.com>
2025-05-03 21:24:02 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml;
+
+namespace LssToYouTubeChapters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Path to *.lss");
+            Console.WriteLine("Drag-and-drop *lss. file and press Enter");
+            string lssPath = Console.ReadLine();
+
+            XmlDocument lssDocument = new XmlDocument();
+            lssDocument.Load(lssPath.Trim('"'));
+
+            XmlNodeList lssNodes = lssDocument.SelectNodes("/Run/Segments/Segment/Name | /Run/Segments/Segment/SplitTimes/SplitTime/RealTime");
+
+            List<string> names = new List<string>();
+            List<string> splitsName = new List<string>();
+            List<double> splitsRealTime = new List<double>();
+
+            foreach (XmlNode node in lssNodes)
+            {
+                if (node.Name == "Name")
+                {
+                    names.Add(node.InnerText);
+                }
+                else
+                {
+                    splitsName.Add(string.Join(" + ", names));
+                    names.Clear();
+                    splitsRealTime.Add(TimeSpan.Parse(node.InnerText).TotalMilliseconds);
+                }
+            }
+
+            Console.WriteLine("Offset (in ms):");
+            string offsetString = Console.ReadLine();
+
+            double offset = double.Parse(offsetString);
+
+            for (int i = 0; i < splitsRealTime.Count; i++)
+            {
+                splitsRealTime[i] += offset;
+            }
+
+            if (offset == 0)
+            {
+                splitsRealTime.Insert(0, 0);
+                splitsRealTime.RemoveAt(splitsRealTime.Count - 1);
+            }
+            else if (offset > 0)
+            {
+                splitsRealTime.Insert(0, 0);
+                splitsName.Insert(0, "Prep");
+                splitsRealTime.Insert(1, offset);
+                splitsRealTime.RemoveAt(splitsRealTime.Count - 1);
+            }
+
+            Console.WriteLine("Time format");
+            Console.WriteLine("1) h:mm:ss");
+            Console.WriteLine("2) mm:ss");
+
+            string timeFormat = Console.ReadLine();
+            string timeFormatString = timeFormat == "1" ? "h':'mm':'ss" : "mm':'ss";
+
+            if (splitsName.Count == splitsRealTime.Count)
+            {
+                for (int i = 0; i < splitsRealTime.Count; i++)
+                {
+                    Console.WriteLine($"{TimeSpan.FromMilliseconds(splitsRealTime[i]).ToString(timeFormatString)} {splitsName[i]}");
+                }
+            }
+            else
+            {
+                Console.WriteLine($"Oops different list sizes. splitsName.Count={splitsName.Count} splitsRealTime.Count={splitsRealTime.Count}");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
